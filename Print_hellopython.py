81ebb4cbf63c0397de191588c0a20ca5159f1e3d
karthik0719 <104984916+karthik0719@users.noreply.github.com>
2022-05-05 17:00:34 +0530

    Add files via upload

---
+++ Print_hellopython.py
@@ -0,0 +1,36 @@
+"""
+Name: Karthik
+Date & Day: 5th May 2022 , Thursday
+Time:8:00 AM
+File Description: print statement and types of comments
+"""
+
+
+
+
+""" To print  hello Karthik   """
+# print('hello Karthik')
+# print("hello Karthik")
+# print('''hello Karthik''')
+# print("""hello Karthik""")
+
+
+""" To print  hello wor'ld   """
+# print('hello wor\'ld')
+# print("hello wor'ld")
+# print('''hello wor'ld''')
+# print("""hello wor'ld""")
+
+""" To print  hello wor"ld   """
+# print('hello wor"ld')
+# print("hello wor\"ld")
+# print('''hello wor"ld''')
+# print("""hello wor"ld""")
+
+print('print(hello python)')
+print("print(hello python)")
+print('''print(hello python)''')
+print("""print(hello python)""")
+
+
+
